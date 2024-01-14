5e0eb85be242b2757e47b8235968da440e4a1cf7
jmorr1190 <jmorr1190@gmail.com>
2024-01-13 19:12:28 -0800

    created along with class

---
+++ Week9.js
@@ -0,0 +1,17 @@
+
+const value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" ]; 
+
+const suite = ["diamonds", "hearts", "spades", "clubs"]
+
+
+class Card {
+    constructor(suit, value){
+        this.suit = suit;
+        this.value = value; 
+    }
+}
+
+
+// every value you need to assign an suit can be done with a double for loop
+
+//https://www.youtube.com/watch?v=b8GF-eiACzs// stopped at 1.20
